ec1c500b66d9dc8dc1c7199a5a93779d7bca007b
tomnetgeneraltradingplc <tomnetgeneraltradingplc@gmail.com>
2019-06-22 15:49:23 -0500

    These two questions are from my job interview.

--- src/HelloWorld/ReverseArrayWithoutStringMethod.java
+++ src/HelloWorld/ReverseArrayWithoutStringMethod.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 public class ReverseArrayWithoutStringMethod {
     public static void main(String[] args) {
    int[] num =  {1,2,3,4};
-        for (int i=3;i>=0;i--){
+        for (int i=num.length-1;i>=0;i--){
         System.out.print(num[i]);
 
     }
